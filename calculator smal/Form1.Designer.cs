5baa9cfb68144474845f5e47b112d92b2f00528d
saronorozi <saronorozi8130@gmail.com>
2022-09-13 13:58:07 +0430

    first commit

---
+++ calculator smal/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace calculator_smal
+{
+    partial class calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtmumber1 = new System.Windows.Forms.TextBox();
+            this.txtnumber2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblsum = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "the show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "number1:";
+            // 
+            // txtmumber1
+            // 
+            this.txtmumber1.Location = new System.Drawing.Point(122, 78);
+            this.txtmumber1.Name = "txtmumber1";
+            this.txtmumber1.Size = new System.Drawing.Size(59, 26);
+            this.txtmumber1.TabIndex = 2;
+            // 
+            // txtnumber2
+            // 
+            this.txtnumber2.Location = new System.Drawing.Point(122, 128);
+            this.txtnumber2.Name = "txtnumber2";
+            this.txtnumber2.Size = new System.Drawing.Size(59, 26);
+            this.txtnumber2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "number2:";
+            // 
+            // lblsum
+            // 
+            this.lblsum.AutoSize = true;
+            this.lblsum.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblsum.Location = new System.Drawing.Point(122, 210);
+            this.lblsum.Name = "lblsum";
+            this.lblsum.Size = new System.Drawing.Size(18, 20);
+            this.lblsum.TabIndex = 5;
+            this.lblsum.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "sum:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(218, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "بعلاوه+";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(336, 431);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblsum);
+            this.Controls.Add(this.txtnumber2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtmumber1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "calculator";
+            this.Text = "calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtmumber1;
+        private System.Windows.Forms.TextBox txtnumber2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblsum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ calculator smal/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator_smal
+{
+    public partial class calculator : Form
+    {
+        Int32 x1,x2,sum;
+        public calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x1 = Convert.ToInt32(txtmumber1.Text);
+            x2 = Convert.ToInt32(txtnumber2.Text);
+            sum = x1 + x2;
+            lblsum.Text = Convert.ToString(sum);
+           // Convert.ToInt32();
+           // Convert.ToString();
+           
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
